d36b89db0e192bd5ea0c7d43c1ab8328d6e057d3
zhangwanqiang <412419407@qq.com>
2021-02-05 09:57:19 +0800

    生成数据库设计文档

---
+++ src/main/java/com/zcj/config/SpringConfig.java
@@ -0,0 +1,33 @@
+package com.zcj.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+@ComponentScan(basePackages= {"com.zcj"})
+@PropertySource("classpath:config/db.properties")
+public class SpringConfig {
+	@Value("${db_url}")
+	private String url;
+	@Value("${db_username}")
+	private String username;
+	@Value("${db_password}")
+	private String password;
+	@Value("${db_driver}")
+	private String driver;
+	@Bean
+	public DriverManagerDataSource driverManagerDataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource(url, username, password);
+		dataSource.setDriverClassName(driver);
+		return dataSource;
+	}
+	@Bean
+	public JdbcTemplate jdbcTemplate() {
+		return new JdbcTemplate(driverManagerDataSource());
+	}
+}
